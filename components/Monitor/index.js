93cdd134f1d3ba6f653f74aa2bdc1459b86a64e8
Nhan vo <nhanvohmd1619@gmail.com>
2023-03-10 13:16:52 +0700

    fix select tree map, in progress map monitor

--- components/Monitor/index.js
+++ components/Monitor/index.js
@@ -1,26 +1,51 @@
 import {useEffect, useState} from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native"
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
-import MapView, { Callout, Circle, Marker } from "react-native-maps"
+import {Dimensions, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View} from "react-native"
+import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete"
+import MapView, {Callout, Circle, LocalTile, Marker, Overlay, UrlTile} from "react-native-maps"
 import dataEvents from '../../utils/dummyData/eventList.json';
 import dataIOTDevicesConfig from '../../utils/dummyData/managementIOTDeviceConfig.json';
 import dataCameraDevices from '../../utils/dummyData/managementCameraDevice.json';
 import dataIOTDevices from '../../utils/dummyData/managementIOTDevice.json';
 import dataBuildings from '../../utils/dummyData/managementBuilding.json';
 import dataFloors from '../../utils/dummyData/managementFloor.json';
-import data from '../../utils/dummyData/treeData.json';
+import treeselectData from '../../utils/dummyData/treeData.json';
 import {generateDropdownTreeSelectMap} from "../../utils/helper/helper";
-import DropdownTreeSelect from "react-dropdown-tree-select";
+import TreeSelect from 'react-native-tree-select';
+import mapImage from '../../assets/imageTest/floor2_B.png'
+import mapImage1 from '../../assets/imageTest/buildingA.png'
+import mapImage2 from '../../assets/imageTest/floor4_A.png'
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+const OverlayComponent = () => {
+    return (
+        <View style={{}}>
+            <ImageBackground style={{
+                width: screenWidth * 100 / 100,
+                height: screenHeight * 60 / 100,
+                display: 'flex',
+                resizeMode: 'contain',
+            }}
+                             source={mapImage1}
+                             scale={0.5}
+            />
+
+        </View>
+    )
+}
+
 
 export default function Monitor({navigation}) {
     const [dataTree, setDataTree] = useState([]);
-    const [ pin, setPin ] = useState({
+    const [result, setResult] = useState('');
+    const [pin, setPin] = useState({
         // latitude: 37.78825,
         // longitude: -122.4324
         latitude: 10.762622,
         longitude: 106.660172
     })
-    const [ region, setRegion ] = useState({
+    const [region, setRegion] = useState({
         // latitude: 37.78825,
         // longitude: -122.4324,
         latitude: 10.76,
@@ -28,37 +53,183 @@ export default function Monitor({navigation}) {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     })
-    const onChange = (currentNode, selectedNodes) => {
-        console.log("path::", currentNode.path);
-    };
-    const assignObjectPaths = (obj, stack) => {
-        Object.keys(obj).forEach(k => {
-            const node = obj[k];
-            if (typeof node === "object") {
-                node.path = stack ? `${stack}.${k}` : k;
-                assignObjectPaths(node, node.path);
-            }
-        });
+    const _onClick = ({item, routes, currentNode}) => {
+        // console.log("item: ", item.type)
+        // console.log("current node: ", currentNode)
+        console.log(item.name, currentNode);
+        console.log(routes);
+        setResult(item.name);
+    }
+    const _onClickLeaf = ({item, routes, currentNode}) => {
+        console.log(item.name, currentNode);
+        console.log(routes);
     };
 
 
     useEffect(() => {
-        // let data = generateDropdownTreeSelectMap(dataBuildings, dataFloors, dataCameraDevices, dataIOTDevices);
-        // console.log("data monitor: ", data);
-        // assignObjectPaths(treeData);
-        // setDataTree(data);
+        let combineData = generateDropdownTreeSelectMap(dataBuildings, dataFloors, dataCameraDevices, dataIOTDevices);
+        setDataTree(combineData["_j"]);
     }, [])
 
-    assignObjectPaths(data)
 
     return (
-        <View style={{ marginTop: 50, flex: 1 }}>
+        <View style={{marginTop: 10, flex: 1}}>
+            <Text>{result}</Text>
 
-            <DropdownTreeSelect
-                data={data}
-                onChange={onChange}
-                mode="radioSelect"
-            />
+            {/*<View style={{*/}
+            {/*    width: '100%',*/}
+            {/*    height: screenHeight / 50,*/}
+            {/*    // flex: 1,*/}
+            {/*    // marginBottom: 20,*/}
+            {/*    backgroundColor: 'red'*/}
+            {/*}}>*/}
+            {/*    <TreeSelect*/}
+            {/*        // data={treeselectData}*/}
+            {/*        data={dataTree}*/}
+            {/*        isOpen={true}*/}
+            {/*        // openIds={['A01']}*/}
+            {/*        // defaultSelectedId={['B062']}*/}
+            {/*        isShowTreeId={false}*/}
+            {/*        // selectType="single"*/}
+            {/*        selectType="single"*/}
+            {/*        itemStyle={{*/}
+            {/*            backgroudColor: 'white',*/}
+            {/*            fontSize: 12,*/}
+            {/*            color: '#995962'*/}
+            {/*        }}*/}
+            {/*        selectedItemStyle={{*/}
+            {/*            backgroudColor: 'yellow',*/}
+            {/*            fontSize: 16,*/}
+            {/*            color: '#171e99'*/}
+            {/*        }}*/}
+            {/*        onClick={_onClick}*/}
+            {/*        onClickLeaf={_onClickLeaf}*/}
+            {/*        treeNodeStyle={{*/}
+            {/*            openIcon: <Image*/}
+            {/*                resizeMode="stretch"*/}
+            {/*                style={{width: 18, height: 18}}*/}
+            {/*                // source={{uri: 'https://freedesignfile.com/upload/2016/05/House-building-blueprint-design-vector-04.jpg'}}*/}
+            {/*                source={mapImage}*/}
+            {/*            />,*/}
+            {/*            closeIcon: <Image*/}
+            {/*                resizeMode="stretch"*/}
+            {/*                style={{width: 18, height: 18}}*/}
+            {/*                source={{uri: 'https://freedesignfile.com/upload/2016/05/House-building-blueprint-design-vector-04.jpg'}}*/}
+            {/*            />,*/}
+            {/*        }}*/}
+            {/*    />*/}
+            {/*</View>*/}
+
+            <View>
+                {/*<MapView*/}
+                {/*    style={styles.map}*/}
+                {/*    initialRegion={{*/}
+                {/*        // latitude: 37.78825,*/}
+                {/*        // longitude: -122.4324,*/}
+                {/*        latitude: 10.762622,*/}
+                {/*        longitude: 106.660172,*/}
+                {/*        latitudeDelta: 0.0922,*/}
+                {/*        longitudeDelta: 0.0421*/}
+                {/*    }}*/}
+                {/*    provider="google"*/}
+                {/*>*/}
+                {/*    /!*<Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />*!/*/}
+                {/*    <Marker*/}
+                {/*        coordinate={pin}*/}
+                {/*        pinColor="black"*/}
+                {/*        // draggable={true}*/}
+                {/*        // onDragStart={(e) => {*/}
+                {/*        //     console.log("Drag start", e.nativeEvent.coordinates)*/}
+                {/*        // }}*/}
+                {/*        // onDragEnd={(e) => {*/}
+                {/*        //     setPin({*/}
+                {/*        //         latitude: e.nativeEvent.coordinate.latitude,*/}
+                {/*        //         longitude: e.nativeEvent.coordinate.longitude*/}
+                {/*        //     })*/}
+                {/*        // }}*/}
+                {/*        title='marker ne'*/}
+                {/*        description='description marker'*/}
+                {/*    >*/}
+                {/*        <Callout>*/}
+                {/*            <Text>nhan v</Text>*/}
+                {/*        </Callout>*/}
+                {/*    </Marker>*/}
+                {/*    /!*<Circle center={pin} radius={1000} />*!/*/}
+                {/*</MapView>*/}
+
+
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        // latitude: 37.78825,
+                        // longitude: -122.4324,
+                        latitude: 10.762622,
+                        longitude: 106.660172,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    }}
+                    // provider="google"
+                >
+                    {/*<Marker*/}
+                    {/*    coordinate={pin}*/}
+                    {/*    pinColor="black"*/}
+                    {/*    title='marker ne'*/}
+                    {/*    description='description marker'*/}
+                    {/*>*/}
+                    {/*    <Callout>*/}
+                    {/*        <Text>nhan v</Text>*/}
+                    {/*    </Callout>*/}
+                    {/*</Marker>*/}
+
+                    {/*<OverlayComponent />*/}
+                    {/*<Marker*/}
+                    {/*    coordinate={pin}*/}
+                    {/*    pinColor="black"*/}
+                    {/*    title='marker ne'*/}
+                    {/*    description='description marker'*/}
+                    {/*    style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}*/}
+                    {/*>*/}
+                    {/*    <Callout>*/}
+                    {/*        <Text>nhan v</Text>*/}
+                    {/*    </Callout>*/}
+                    {/*</Marker>*/}
+
+
+                    <Overlay bounds={[[10.762622, 106.660172], [10.765622, 106.663172]]} image={mapImage} />
+                </MapView>
+
+                {/*<View*/}
+                {/*    pointerEvents="none"*/}
+                {/*    style={{*/}
+                {/*        position: 'absolute', top: 0, left: 0*/}
+                {/*    }}>*/}
+                {/*    <Image style={{*/}
+                {/*        display: 'flex',*/}
+                {/*        resizeMode: 'contain',*/}
+                {/*        width: screenWidth,*/}
+                {/*        height: screenHeight*/}
+                {/*    }} source={mapImage1}/>*/}
+
+                {/*</View>*/}
+
+                {/*<View*/}
+                {/*    pointerEvents="none"*/}
+                {/*    style={{*/}
+                {/*        position: 'absolute', top: 10.762622, left: 106.660172*/}
+                {/*    }}>*/}
+                {/*    <Image style={{*/}
+                {/*        display: 'flex',*/}
+                {/*        resizeMode: 'contain',*/}
+                {/*        width: screenWidth * 20 / 100,*/}
+                {/*        height: screenHeight * 20 / 100,*/}
+                {/*    }} source={mapImage2}/>*/}
+
+                {/*</View>*/}
+
+
+                {/*<OverlayComponent/>*/}
+
+            </View>
 
 
             {/*<GooglePlacesAutocomplete*/}
@@ -92,39 +263,6 @@ export default function Monitor({navigation}) {
             {/*/>*/}
 
 
-            <MapView
-                style={styles.map}
-                initialRegion={{
-                    // latitude: 37.78825,
-                    // longitude: -122.4324,
-                    latitude: 10.762622,
-                    longitude: 106.660172,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                }}
-                provider="google"
-            >
-                {/*<Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />*/}
-                <Marker
-                    coordinate={pin}
-                    pinColor="black"
-                    draggable={true}
-                    onDragStart={(e) => {
-                        console.log("Drag start", e.nativeEvent.coordinates)
-                    }}
-                    onDragEnd={(e) => {
-                        setPin({
-                            latitude: e.nativeEvent.coordinate.latitude,
-                            longitude: e.nativeEvent.coordinate.longitude
-                        })
-                    }}
-                >
-                    <Callout>
-                        <Text>I'm here</Text>
-                    </Callout>
-                </Marker>
-                {/*<Circle center={pin} radius={1000} />*/}
-            </MapView>
         </View>
     )
 }
@@ -137,5 +275,13 @@ const styles = StyleSheet.create({
     map: {
         width: '100%',
         height: '100%',
+    },
+    overlay: {
+        position: 'absolute',
+        bottom: 50,
+        top: 50,
+        left: 50,
+        right: 50,
+        backgroundColor: 'red',
     },
 });
--- utils/helper/helper.js
+++ utils/helper/helper.js
@@ -9,11 +9,31 @@ export const convertDate = (date) => {
 }
 
 export const generateDropdownTreeSelectMap = async (dataBuildings, dataFloors, dataCameraDevices, dataIOTDevices) => {
+    let devicesList = dataCameraDevices.concat(dataIOTDevices);
     let result = [];
-    for (let i = 0; i < dataBuildings; i++) {
+    for (let i = 0; i < dataBuildings.length; i++) {
         let building = dataBuildings[i];
-
-        result.push({...building, label: building.name});
+        let childrenOfBuilding = [];
+        // floors
+        for (let j = 0; j < dataFloors.length; j++) {
+            let floor = dataFloors[j];
+            if (floor.building_id == building.id) {
+                let childrenOfFloor = [];
+                for (let k = 0; k < devicesList.length; k++) {
+                    if (devicesList[k].building_id == building.id && devicesList[k].floor_level == floor.floor_level) {
+                        childrenOfFloor.push({...devicesList[k], id: devicesList[k].id, name: devicesList[k].name, sortNo: devicesList[k].id, parentId: floor.id, children: []})
+                    }
+                }
+                childrenOfBuilding.push({...floor, id: floor.id, name: floor.name, sortNo: floor.id, parentId: building.id, children: childrenOfFloor})
+            }
+        }
+        // devices not belong to floor
+        for (let k = 0; k < devicesList.length; k++) {
+            if (devicesList[k].building_id == building.id && devicesList[k].floor_level == -1) {
+                childrenOfBuilding.push({...devicesList[k], id: devicesList[k].id, name: devicesList[k].name, sortNo: devicesList[k].id, parentId: building.id, children: []})
+            }
+        }
+        result.push({...building, id: building.id, name: building.name, sortNo: building.id, parentId: "", children: childrenOfBuilding});
     }
 
     return result;
